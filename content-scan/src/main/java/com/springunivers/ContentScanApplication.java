a5d92ffa782557c564a9ac448743f93855280f2e
Gleyson Sampaio <gzynho@hotmail.com>
2019-05-03 19:21:28 -0300

    INICIANDO A IMPLEMENTACAO VIA DA LEITURA DE DIRETORIO

--- content-scan/src/main/java/com/springunivers/ContentScanApplication.java
+++ content-scan/src/main/java/com/springunivers/ContentScanApplication.java
@@ -10,13 +10,14 @@ import com.springunivers.start.ContentScanPrograma;
 @SpringBootApplication
 public class ContentScanApplication {
 	public static void main(String[] args) {
-		System.setProperty("app.home", System.getProperty("user.dir"));
+		System.setProperty("app.path", ContentScanPrograma.APP_PATH);
+		System.setProperty("scan.log", "scan");
 		SpringApplication.run(ContentScanApplication.class, args);
 	}
 	@Bean
 	public CommandLineRunner run(ContentScanPrograma programa) {
 		return args -> {
-			programa.validarExtensa();
+			programa.executar();
 		};
 	}
 }
--- content-scan/src/main/java/com/springunivers/start/ContentScanPrograma.java
+++ content-scan/src/main/java/com/springunivers/start/ContentScanPrograma.java
@@ -1,6 +1,7 @@
 package com.springunivers.start;
 
 import java.io.File;
+import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -8,19 +9,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.springunivers.converter.Extensao;
+import com.springunivers.util.FileReaderUtil;
 
 @Component
 public class ContentScanPrograma {
-	private static final Logger logger = LogManager.getLogger(ContentScanPrograma.class);
+	public static final String APP_PATH = System.getProperty("user.dir");
+	private static final Logger LOGGER = LogManager.getLogger(ContentScanPrograma.class);
+	private List<String> scanDiretorios;
 	@Autowired
 	private Extensao extensao;
+
 	public void validarExtensa() {
 		File file = new File("C:\\tci\\img\\3130\\1.tif");
 		try {
 			System.out.println(extensao.extensaoOriginal(file));
-			logger.info("FIM");
+			LOGGER.info("FIM");
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+
+	public void executar() {
+		try {
+			File diretorios = new File(APP_PATH, "diretorios.txt");
+			if(!diretorios.exists())
+				throw new RuntimeException("Não foi localizado o arquivo " + diretorios.getAbsolutePath());
+			FileReaderUtil fileReader = new FileReaderUtil();
+			scanDiretorios = fileReader.readLine(diretorios);
+		} catch (Exception e) {
+			e.printStackTrace();
+			LOGGER.error(e.getMessage());
 		}
 	}
 }
