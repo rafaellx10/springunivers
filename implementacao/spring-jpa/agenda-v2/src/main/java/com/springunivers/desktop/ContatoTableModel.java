1bba834d8ccce7afbd8606c71f52b824a89130fa
Gleyson Sampaio <gzynho@hotmail.com>
2019-05-28 19:31:23 -0300

    v2

---
+++ implementacao/spring-jpa/agenda-v2/src/main/java/com/springunivers/desktop/ContatoTableModel.java
@@ -0,0 +1,84 @@
+package com.springunivers.desktop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+import com.springunivers.model.Contato;
+
+public class ContatoTableModel extends AbstractTableModel {
+	private static final int COL_ID = 0;
+    private static final int COL_NOME = 1;
+    private static final int COL_DDD = 3;
+    private static final int COL_TELEFONE = 2;
+    
+    private List<Contato> linhas;
+    private String[] colunas = new String[]{"Id", "Nome", "DDD", "Telefone"};
+    
+    public static ContatoTableModel vazio() {
+    	return new ContatoTableModel(new ArrayList<Contato>());
+    }
+    
+    public ContatoTableModel(List<Contato> contatos) {
+        this.linhas = new ArrayList<>(contatos);
+    }
+    
+    public int getRowCount() {
+        return linhas.size();
+    }
+ 
+    public int getColumnCount() {
+        return colunas.length;
+    }
+ 
+    public String getColumnName(int columnIndex) {
+        return colunas[columnIndex];
+    }
+ 
+    public Class getColumnClass(int columnIndex) {
+        if (columnIndex == COL_ID) {
+            return Integer.class;
+        }
+        return String.class;
+    }
+ 
+    public boolean isCellEditable(int rowIndex, int
+    columnIndex) {
+        return false;
+    }
+ 
+    public Object getValueAt(int row, int column) {
+ 
+        Contato m = linhas.get(row);
+ 
+        if (column == COL_ID) {
+            return m.getId();
+        } else if (column == COL_NOME) {
+            return m.getNome();
+        } else if (column == COL_TELEFONE) {
+            return m.getNumero();
+        } else if (column == COL_DDD) {
+            return m.getDdd();
+        }
+        return "";
+    }
+ 
+    public void setValueAt(Object aValue, int row, 
+    int column) {
+        Contato u = linhas.get(row);
+        if (column == COL_ID) {
+            u.setId((Integer) aValue);
+        } else if (column == COL_NOME) {
+            u.setNome(aValue.toString());
+        } else if (column == COL_DDD) {
+            u.setDdd(Integer.valueOf(aValue.toString()));
+        } else if (column == COL_TELEFONE) {
+            u.setNumero(Long.valueOf(aValue.toString()));
+        }
+    }
+ 
+    public Contato getContato(int indiceLinha) {
+        return linhas.get(indiceLinha);
+    }
+}
---
+++ implementacao/spring-jpa/agenda-v2/src/main/java/com/springunivers/model/Contato.java
@@ -0,0 +1,76 @@
+package com.springunivers.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_contato")
+public class Contato {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+	@Column(length=50,nullable=false)
+	private String nome;
+	@Column(length=50,nullable=false)
+	private String sobrenome;
+	@Column(length=2,nullable=false)
+	private Integer ddd;
+	@Column(length=9,nullable=false)
+	private Long numero;
+	@Column(length=50,nullable=false)
+	private String cidade;
+	@Column(length=2,nullable=false)
+	private String estado;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	public Integer getDdd() {
+		return ddd;
+	}
+	public void setDdd(Integer ddd) {
+		this.ddd = ddd;
+	}
+	public Long getNumero() {
+		return numero;
+	}
+	public void setNumero(Long numero) {
+		this.numero = numero;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	@Override
+	public String toString() {
+		return "Contato [id=" + id + ", nome=" + nome + ", sobrenome=" + sobrenome + ", ddd=" + ddd + ", numero="
+				+ numero + ", cidade=" + cidade + ", estado=" + estado + "]";
+	}
+	
+}
