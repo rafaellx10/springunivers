175221c48001ea1e518b8dd8e3dc18b97cc5a7be
Gleyson Sampaio <gzynho@hotmail.com>
2019-05-29 15:57:53 -0300

    joined

---
+++ implementacao/spring-jpa/agenda-v2/src/main/java/com/springunivers/model/heranca/joined/Cliente.java
@@ -0,0 +1,33 @@
+package com.springunivers.model.heranca.joined;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="h4_cliente")
+public class Cliente extends Contato {
+	@Temporal(TemporalType.TIMESTAMP)
+	//@Temporal(TemporalType.DATE)
+	private Date ultimaCompra;
+	@Column(length = 7,precision = 2)
+	private Double valor;
+	public Date getUltimaCompra() {
+		return ultimaCompra;
+	}
+	public void setUltimaCompra(Date ultimaCompra) {
+		this.ultimaCompra = ultimaCompra;
+	}
+	public Double getValor() {
+		return valor;
+	}
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	
+	
+}
---
+++ implementacao/spring-jpa/agenda-v2/src/main/java/com/springunivers/model/heranca/joined/Fornecedor.java
@@ -0,0 +1,23 @@
+package com.springunivers.model.heranca.joined;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+@Entity
+@Table(name="h4_fornecedor")
+public class Fornecedor extends Contato {
+	@Temporal(TemporalType.DATE)
+	private Date ultimaVisita;
+
+	public Date getUltimaVisita() {
+		return ultimaVisita;
+	}
+
+	public void setUltimaVisita(Date ultimaVisita) {
+		this.ultimaVisita = ultimaVisita;
+	}
+	
+}
