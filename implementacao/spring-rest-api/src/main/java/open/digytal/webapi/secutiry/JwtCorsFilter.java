ce72db389c4f4cecb9a9854df89b7b4a15127f5f
Gleyson <gzynho@hotmail.com>
2019-07-16 00:03:13 -0300

    erro de cors

--- implementacao/spring-rest-api/src/main/java/open/digytal/webapi/secutiry/JwtCorsFilter.java
+++ implementacao/spring-rest-api/src/main/java/open/digytal/webapi/secutiry/JwtCorsFilter.java
@@ -10,7 +10,9 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Component;
 
+@Component
 public class JwtCorsFilter implements Filter {
 
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
---
+++ implementacao/vue-app/src/components/external/Login.vue
@@ -0,0 +1,53 @@
+<template>
+<div>
+
+<div style="margin: 20px;"></div>
+<el-form label-width="100px" :model="form">
+  <el-form-item label="Username">
+    <el-input v-model="form.username"></el-input>
+  </el-form-item>
+  <el-form-item label="Password">
+    <el-input v-model="form.password"></el-input>
+  </el-form-item>
+  <el-form-item>
+    <el-button type="primary" @click="logar()" >Login</el-button>
+  </el-form-item>
+</el-form>
+
+</div>
+</template>
+<script>
+  import axios from 'axios';
+  export default {
+    data() {
+      return {
+        data: [],
+        labelPosition: 'right',
+        form: {
+          username: '',
+          password: ''
+        }
+      }
+    },
+    mounted () {
+      
+  },
+  methods: {
+    submitForm() {
+        console.log(this.form.username);
+      },
+    logar () {
+        console.log(this.form.username + "---"+ this.form.password)
+       axios.post('http://localhost:8081/login',{"username":this.form.username,"password":this.form.password}
+       ).then(({ data }) => {
+          console.log(data);
+          //LocalStorage.set('token', data.token);
+          //this.$router.push({path: '/app'})
+        }).catch(({ error }) => {
+           console.log("ERRO");
+          console.log(error);
+        }).finally(() => {});
+      }
+    }
+  };
+</script>
