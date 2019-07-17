bf61879266f4a0857316841d75e975d21ab53e4f
Gleyson <gzynho@hotmail.com>
2019-07-16 22:14:27 -0300

    CONSULTA DE CONTATOS

--- implementacao/vue-app/src/components/app/Contatos.vue
+++ implementacao/vue-app/src/components/app/Contatos.vue
@@ -1,6 +1,12 @@
 <template>
 <div>
-  CONTATOS
+  <b-button variant="primary" v-on:click="listar">Buscar</b-button>
+  <div>
+    <b-table striped hover :items="items"></b-table>
+  </div>
+  <div>
+    <b-table striped hover :items="items" :fields="fields"></b-table>
+  </div>
 </div>
 </template>
 <script>
@@ -8,13 +14,47 @@
   export default {
     data() {
       return {
-        
+        items: [],
+        fields: [
+          {
+            key: 'id',
+            sortable: true
+          },
+          {
+            key: 'nome',
+            sortable: true
+          },
+          ,
+          {
+            key: 'sobrenome',
+            sortable: true
+          },
+          {
+            key: 'telefone.ddd',
+            label: 'DDD'
+          },
+          {
+            key: 'telefone.numero',
+            label: 'Numero'
+          }
+        ]
       }
     },
     mounted () {
       
   },
   methods: {
+    listar(){
+        let token=  localStorage.getItem('token');
+        console.log(token);
+        axios.get('http://localhost:8081/cadastro/contatos',{headers: {'Authorization': token}})
+        .then(({ data }) => {
+          console.log(data);
+          this.items = data;
+        }).catch(({ err }) =>{
+          console.log(err);  
+        }).finally(() => {})
+      }
     }
   };
 </script>
--- implementacao/vue-app/src/components/app/Main.vue
+++ implementacao/vue-app/src/components/app/Main.vue
@@ -1,8 +1,24 @@
 <template>
-    <div>
-      MAIN
+     <div>
+    <b-container>
+      <b-jumbotron
+        header="Tela do Sistema"
+        lead="Demonstração dos recursos de Vue.js (Rotas, APIs e Etc)"
+      >
+        <p>Template legal: https://github.com/coreui/coreui-free-vue-admin-template</p>
+        <b-nav>
+          <b-nav-item active>
+            <router-link to="/contatos">Listar Contatos</router-link>
+          </b-nav-item>
+           <b-nav-item active>
+            <router-link to="/login">Logoff</router-link>
+          </b-nav-item>
+        </b-nav>
+      </b-jumbotron>
+
       <router-view />
-    </div>
+    </b-container>
+  </div>
    
 </template>
 <script>
